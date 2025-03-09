aefc3e66d59d73796ac21715329afccd1914c604
Ganesh1611 <Ganeshshankar1611@gmail.com>
2025-03-10 01:02:51 +0530

    Update Staging_schema_to_final_table.sql

--- Staging_schema_to_final_table.sql
+++ Staging_schema_to_final_table.sql
@@ -20,67 +20,6 @@ WHERE table_schema = 'staging'
 ORDER BY table_name;
 
 
-
-
--- Example: Normalizing hier_clnd (further normalization could be done)
-CREATE TABLE staging.calendar_day (
-    fscldt_id VARCHAR(255) PRIMARY KEY,
-    fscldt_label VARCHAR(255),
-    date DATE,
-    fscldow INT,
-    fscldom INT,
-    fscldoq INT,
-    fscldoy INT
-);
-
-CREATE TABLE staging.calendar_week (
-    fsclwk_id VARCHAR(255) PRIMARY KEY,
-    fsclwk_label VARCHAR(255),
-    fsclwoy INT
-);
-
-CREATE TABLE staging.calendar_month (
-    fsclmth_id VARCHAR(255) PRIMARY KEY,
-    fsclmth_label VARCHAR(255),
-    fsclmoy INT
-);
-
-CREATE TABLE staging.calendar_quarter (
-    fsclqrtr_id VARCHAR(255) PRIMARY KEY,
-    fsclqrtr_label VARCHAR(255),
-    fsclqoy INT
-);
-
-CREATE TABLE staging.calendar_year (
-    fsclyr_id VARCHAR(255) PRIMARY KEY,
-    fsclyr_label VARCHAR(255)
-);
-
-CREATE TABLE staging.calendar_season(
-    ssn_id VARCHAR(255) PRIMARY KEY,
-    ssn_label VARCHAR(255)
-);
-
-INSERT INTO staging.calendar_day SELECT fscldt_id, fscldt_label, CAST(date AS DATE), CAST(fscldow AS INT), CAST(fscldom AS INT), CAST(fscldoq AS INT), CAST(fscldoy AS INT) FROM staging.hier_clnd_staging;
-INSERT INTO staging.calendar_week SELECT fsclwk_id, fsclwk_label, CAST(fsclwoy AS INT) FROM staging.hier_clnd_staging GROUP BY fsclwk_id, fsclwk_label, fsclwoy;
-INSERT INTO staging.calendar_month SELECT fsclmth_id, fsclmth_label, CAST(fsclmoy AS INT) FROM staging.hier_clnd_staging GROUP BY fsclmth_id, fsclmth_label, fsclmoy;
-INSERT INTO staging.calendar_quarter SELECT fsclqrtr_id, fsclqrtr_label, CAST(fsclqoy AS INT) FROM staging.hier_clnd_staging GROUP BY fsclqrtr_id, fsclqrtr_label, fsclqoy;
-INSERT INTO staging.calendar_year SELECT fsclyr_id, fsclyr_label FROM staging.hier_clnd_staging GROUP BY fsclyr_id, fsclyr_label;
-INSERT INTO staging.calendar_season SELECT ssn_id, ssn_label FROM staging.hier_clnd_staging GROUP BY ssn_id, ssn_label;
-
--- Update fact_transactions_staging to use the normalized calendar tables.
-ALTER TABLE staging.fact_transactions_staging ADD COLUMN calendar_day_id VARCHAR(255);
-UPDATE staging.fact_transactions_staging SET calendar_day_id = fscldt_id;
-ALTER TABLE staging.fact_transactions_staging DROP COLUMN fscldt_id;
-ALTER TABLE staging.fact_transactions_staging ADD CONSTRAINT fk_calendar_day FOREIGN KEY (calendar_day_id) REFERENCES staging.calendar_day(fscldt_id);
-
---Update fact_averagecosts_staging to use the normalized calendar tables.
-ALTER TABLE staging.fact_averagecosts_staging ADD COLUMN calendar_day_id VARCHAR(255);
-UPDATE staging.fact_averagecosts_staging SET calendar_day_id = fscldt_id;
-ALTER TABLE staging.fact_averagecosts_staging DROP COLUMN fscldt_id;
-ALTER TABLE staging.fact_averagecosts_staging ADD CONSTRAINT fk_calendar_day_average_cost FOREIGN KEY (calendar_day_id) REFERENCES staging.calendar_day(fscldt_id);
-
-
 --- final TABLE
 
 CREATE TABLE mview_weekly_sales AS
@@ -99,4 +38,4 @@ CREATE TABLE mview_weekly_sales AS
 
 
 --Validation
-select * from mview_weekly_sales order by 3
+select * from mview_weekly_sales 
